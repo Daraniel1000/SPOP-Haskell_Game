fb7ab21bccba8258cd8d9b87f2a22fbb2712b6fb
Maciej <daraniel1000@gmail.com>
2021-05-21 14:47:13 +0200

    added functions to change a specific node in the graph and change a node's neighbour array

--- game.hs
+++ game.hs
@@ -9,17 +9,30 @@ type State = (Vertex, [Vertex], Graph, Integer, Vertex)
 
 --get a node from a graph by vertex identifier
 getNode :: Vertex->Graph->Node
+getNode v [] = (-1, [])
 getNode v ((i, n) : rest) | v==i        = (i, n)
                           | otherwise   = getNode v rest
-getNode v [] = (-1, [])
 
 --get node's neighbour array
 neighbours :: Node->[Vertex]
 neighbours (_, neigh) = neigh
 
+--set a node's neighbour array
+setNeighbours (v, _) newNeigh = (v, newNeigh)
+
+--change a node in the graph
+changeNode :: Node->Graph->Graph
+changeNode _ []                       	    = []
+changeNode (v, neigh) ((i, n) : rest) | v == i    = (v, neigh) : rest
+                                      | otherwise = (i, n) : (changeNode (v, neigh) rest)
+
 --move in graph
 move :: State->Vertex->State
 move (place, history, world, moves, goal) goto = (goto, (place:history), world, moves - 1, goal)
+
+--add time
+addTime :: State->Integer->State
+addTime (p, h, w, moves, g) toAdd = (p, h, w, moves + toAdd, g)
 
 --check if victory state is achieved
 isWin :: State->Bool
@@ -52,6 +65,6 @@ getLost state   | getMoves state == [] = False
                 | otherwise            = getLost (move state (head (getMoves state)))
 
 --map, see world.jpg
-worldMap = [(0, [0, 1, 2, 3]), (1, [0, 2, 4]), (2, [0, 1, 6]), (3, [0, 6]), (4, [1, 5]), (5, [4]), (6, [2, 3, 7]), (7, [6])]
+worldMap = [(0, [1, 2, 3]), (1, [0, 2, 4]), (2, [0, 1, 6]), (3, [0, 6]), (4, [1, 5]), (5, [4]), (6, [2, 3, 7]), (7, [6])]
 
 startState = (0, [], worldMap, 7, 7)
